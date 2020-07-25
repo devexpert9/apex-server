b1cf408bac589102ed844c8e3abdd9026b0600e9
root <manmohitindiit@gmail.com>
2020-07-25 10:34:28 +0530

    sds

--- api/controllers/cmspageCtrl.js
+++ api/controllers/cmspageCtrl.js
@@ -2,39 +2,51 @@
 
 var mongoose = require('mongoose'),
 multer  = require('multer'),
+users = mongoose.model('users'),
 cmspages = mongoose.model('cmspages');
 var path = require('path');
 
-
+ 
 // const bcrypt = require('bcrypt');
 //****************  create and edit_cmspages_function ****************************
 exports.add_cmspage = function(req, res) {
-  cmspages.findOne({page: req.body.page, user_id: req.body.user_id }, function(err, doc) {
-    if(doc == null){
-      var newadd = new cmspages({
-            page: req.body.page,
-            data: req.body.data,
-            user_id: req.body.user_id,
-            created_on: new Date()
-          });
-
-          newadd.save(function(err, save) {
-          if(save == null){
-            res.send({
-              error: err,
-              status: 0,
-              data: null
-            });
-          }else{
-            res.send({
-              error: null,
-              status: 1,
-              data: req.body.page + 'has been added successfully!'
-            });
-          }
+  users.find({'email': req.body.data.email , '_id': { $ne: req.body.user_id} }, function(err, doc){
+    if(doc.length > 0){
+      //email already exist
+      res.send({
+        status: 0,
+        data: save,
+        msg:'Try Again'
       });
     }else{
-      cmspages.update({page: req.body.page, user_id: req.body.user_id}, { $set: { page: req.body.page, data: req.body.data}}, {new: true}, function(err, save) {
+      users.update({'_id': req.body.user_id}, {$set: {'email': req.body.data.email, 'name': req.body.data.name} }, {new: true}, function(err, doc1){
+        cmspages.findOne({page: req.body.page, user_id: req.body.user_id }, function(err, doc) {
+          if(doc == null){
+            var newadd = new cmspages({
+                  page: req.body.page,
+                  data: req.body.data,
+                  user_id: req.body.user_id,
+                  created_on: new Date()
+                });
+
+            newadd.save(function(err, save) {
+              if(save == null){
+                res.send({
+                  error: err,
+                  status: 0,
+                  data: null
+                });
+              }else{
+                res.send({
+                  error: null,
+                  status: 1,
+                  data: req.body.page + 'has been added successfully!'
+                });
+              }
+            });
+          }else{
+         
+            cmspages.update({page: req.body.page, user_id: req.body.user_id}, { $set: { page: req.body.page, data: req.body.data}}, {new: true}, function(err, save) {
               if(save == null){
                 res.send({
                   status: 0,
@@ -51,7 +63,10 @@ exports.add_cmspage = function(req, res) {
                 });
               }
             });
-        }
+          }
+        });
+      });
+    }
   });
       
 };
