0745b9b17322018ecb9f747f29be55ab801e93b3
root <manmohitindiit@gmail.com>
2020-07-29 12:05:05 +0530

    asdsa

--- api/controllers/cmspageCtrl.js
+++ api/controllers/cmspageCtrl.js
@@ -9,65 +9,125 @@ var path = require('path');
  
 // const bcrypt = require('bcrypt');
 //****************  create and edit_cmspages_function ****************************
-exports.add_cmspage = function(req, res) {
-  users.find({'email': req.body.data.email , '_id': { $ne: req.body.user_id} }, function(err, doc){
-    if(doc.length > 0){
-      //email already exist
-      res.send({
-        status: 0,
-        data: save,
-        msg:'Try Again'
-      });
-    }else{
-      users.update({'_id': req.body.user_id}, {$set: {'email': req.body.data.email, 'name': req.body.data.name} }, {new: true}, function(err, doc1){
-        cmspages.findOne({page: req.body.page, user_id: req.body.user_id }, function(err, doc) {
-          if(doc == null){
-            var newadd = new cmspages({
-                  page: req.body.page,
-                  data: req.body.data,
-                  user_id: req.body.user_id,
-                  created_on: new Date()
-                });
+exports.add_cmspage = function(req, res)
+{
+  if(req.body.page == 'contact')
+  {
+    users.find({'email': req.body.data.email , '_id': { $ne: req.body.user_id} }, function(err, doc)
+      {
+        if(doc.length > 0)
+        {
+          //email already exist
+          res.send({
+            status: 0,
+            data: save,
+            msg:'Try Again'
+          });
+        }else{
+          users.update({'_id': req.body.user_id}, {$set: {'email': req.body.data.email, 'name': req.body.data.name} }, {new: true}, function(err, doc1)
+          {
+            cmspages.findOne({page: req.body.page, user_id: req.body.user_id }, function(err, doc) 
+            {
+              if(doc == null){
+                var newadd = new cmspages({
+                      page: req.body.page,
+                      data: req.body.data,
+                      user_id: req.body.user_id,
+                      created_on: new Date()
+                    });
 
-            newadd.save(function(err, save) {
-              if(save == null){
-                res.send({
-                  error: err,
-                  status: 0,
-                  data: null
+                newadd.save(function(err, save) {
+                  if(save == null){
+                    res.send({
+                      error: err,
+                      status: 0,
+                      data: null
+                    });
+                  }else{
+                    res.send({
+                      error: null,
+                      status: 1,
+                      data: req.body.page + 'has been added successfully!'
+                    });
+                  }
                 });
               }else{
-                res.send({
-                  error: null,
-                  status: 1,
-                  data: req.body.page + 'has been added successfully!'
+             
+                cmspages.update({page: req.body.page, user_id: req.body.user_id}, { $set: { page: req.body.page, data: req.body.data}}, {new: true}, function(err, save) {
+                  if(save == null){
+                    res.send({
+                      status: 0,
+                      data: save,
+                      msg:'Try Again'
+                    });
+
+                  }else{
+                    res.json({
+                       status: 1,
+                       add:0,
+                       data: save,
+                       msg:'Updated successfully!'
+                    });
+                  }
                 });
               }
             });
-          }else{
-         
-            cmspages.update({page: req.body.page, user_id: req.body.user_id}, { $set: { page: req.body.page, data: req.body.data}}, {new: true}, function(err, save) {
-              if(save == null){
-                res.send({
-                  status: 0,
-                  data: save,
-                  msg:'Try Again'
-                });
+          });
+        }
+      });
+  }
+  else
+  {
+    cmspages.findOne({page: req.body.page, user_id: req.body.user_id }, function(err, doc) 
+    {
+      if(doc == null)
+      {
+        var newadd = new cmspages({
+              page: req.body.page,
+              data: req.body.data,
+              user_id: req.body.user_id,
+              created_on: new Date()
+            });
 
-              }else{
-                res.json({
-                   status: 1,
-                   add:0,
-                   data: save,
-                   msg:'Updated successfully!'
-                });
-              }
+        newadd.save(function(err, save) {
+          if(save == null){
+            res.send({
+              error: err,
+              status: 0,
+              data: null
+            });
+          }else{
+            res.send({
+              error: null,
+              status: 1,
+              data: req.body.page + 'has been added successfully!'
             });
           }
         });
-      });
-    }
-  });
+      }else{
+     
+        cmspages.update({page: req.body.page, user_id: req.body.user_id}, { $set: { page: req.body.page, data: req.body.data}}, {new: true}, function(err, save) {
+          if(save == null){
+            res.send({
+              status: 0,
+              data: save,
+              msg:'Try Again'
+            });
+
+          }else{
+            res.json({
+               status: 1,
+               add:0,
+               data: save,
+               msg:'Updated successfully!'
+            });
+          }
+        });
+      }
+    });
+  }
+
+  
       
 };
 
