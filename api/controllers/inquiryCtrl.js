2f06dea9af5417ed2bf52ed52f228ad441e5e83b
root <manmohitindiit@gmail.com>
2020-08-29 16:22:24 +0530

    server up

--- api/controllers/inquiryCtrl.js
+++ api/controllers/inquiryCtrl.js
@@ -71,7 +71,7 @@ exports.add_contactRequest = function(req, res) {
             var helper = require('sendgrid').mail;
             var fromEmail = new helper.Email('noreply@apex.com','APEX Insurance Services');
             // var toEmail   = new helper.Email(req.body.data.currentAgentEmail);
-            var toEmail  = new helper.Email('manmohitindiit@gmail.com');
+            var toEmail  = new helper.Email('bcc@apex-4u.com');
             var subject = 'Contact Request Submitted';
 
             dynamic_data = dynamic_data.replace("#NAME#", req.body.data.name) ;
