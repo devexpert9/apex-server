c06752f40291c724946d53256409c93014086244
root <manmohitindiit@gmail.com>
2020-09-15 21:42:17 +0530

    sdfs

--- api/controllers/securityprivacyCtrl.js
+++ api/controllers/securityprivacyCtrl.js
@@ -9,59 +9,74 @@ securityprivacy   = mongoose.model('securityprivacy');
 var path      = require('path');
 
 //--- Create SECURITY-----------------------------
-exports.addSecurityData = function(req, res) {
-  var new_pack = new securityprivacy({
-    heading:    req.body.heading,
-    content:    req.body.content,
-    image:      req.body.image,
-    created_at: new Date()
-  });
-  new_pack.save(function(err, doc) 
+exports.addSecurityData = function(req, res)
+{
+  securityprivacy.findOne({}, function(err, doc) 
   {
-    if(doc == null){
-      res.send({
-        data: null,
-        error: 'Something went wrong.Please try later.',
-        status: 0
+    if(doc == null)
+    {
+      var new_pack = new securityprivacy({
+        heading:    req.body.heading,
+        content:    req.body.content,
+        image:      req.body.image,
+        created_at: new Date()
       });
-    }else{
-      res.send({
-        data: doc,
-        status: 1,
-        error: 'data added successfully!'
+      new_pack.save(function(err, doc) 
+      {
+        if(doc == null){
+          res.send({
+            data: null,
+            error: 'Something went wrong.Please try later.',
+            status: 0
+          });
+        }else{
+          res.send({
+            data: doc,
+            status: 1,
+            error: 'data added successfully!'
+          });
+        }
+      });
+    }
+    else
+    {
+      securityprivacy.update({_id: doc._id},{$set:{ 'heading': req.body.heading, 'content': req.body.content, 'image':req.body.image} }, {new: true}, function(err, user)
+      {
+        if(user == null)
+        {
+          res.send({
+            error: err,
+            status: 0,
+            msg:"Try Again"
+          });
+        }
+        else
+        {
+          res.json({
+            error: null,
+            status: 1,
+            data:user,
+            msg:"Testimonial updated successfully!"
+          });
+        }
       });
     }
   });
+
+  
 };
 
 // Update SECURITY -----------------------------------
 exports.updateSecurityData = function(req, res)
 {
-  securityprivacy.update({_id: req.body._id},{$set:{ 'heading': req.body.heading, 'content': req.body.content, 'image':req.body.image} }, {new: true}, function(err, user)
-          {
-            if(user == null)
-            {
-              res.send({
-                error: err,
-                status: 0,
-                msg:"Try Again"
-              });
-            }
-            else
-            {
-              res.json({
-                error: null,
-                status: 1,
-                data:user,
-                msg:"Testimonial updated successfully!"
-              });
-            }
-          });
+  
 };
 
 // GET SECURITY BY ID --------------------------------
-exports.getSecurityPrivacyById = function(req, res) {
-  securityprivacy.findOne({_id:req.body._id}, function(err, user) {
+exports.getSecurityPrivacyById = function(req, res)
+{
+  securityprivacy.findOne({}, function(err, user)
+  {
     if(user == null){
       res.send({
         status: 0,
@@ -72,7 +87,7 @@ exports.getSecurityPrivacyById = function(req, res) {
       res.json({
          status: 1,
          data: user,
-         error:'Testimonial fetched successfully!'
+         error:'Data fetched successfully!'
       });
     }
   });
