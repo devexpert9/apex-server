65ee4d1e21fc256bf773ea369cbb3c7aad037ce4
root <manmohitindiit@gmail.com>
2020-10-08 23:05:30 +0530

    sdsd

--- api/controllers/paymentCtrl.js
+++ api/controllers/paymentCtrl.js
@@ -30,7 +30,21 @@ exports.storeCreditCardVault = function (req, res) {
 	// 		"external_customer_id": uuid.v4()
 	// 	};
 
-	var card_data = {
+	cards.find({userId: req.body.external_customer_id}, function(err, doc)
+  	{
+	    if(doc) //-- If user have any card then delete that card------
+	    {
+      		cards.remove({userId: req.body.external_customer_id}, function(err, user) {
+		      	res.json({
+					status: 1,
+					data: user,
+					error:'Cards deleted successfully!'
+		      	});
+		  	});
+	    }
+	    
+	    //--- After delete user card add new one--------------------
+	    var card_data = {
 			"type": req.body.type,
 			"number": req.body.card_number,
 			"expire_month": req.body.exp_month,
@@ -41,39 +55,42 @@ exports.storeCreditCardVault = function (req, res) {
 			"external_customer_id": uuid.v4()
 		};
 
-	paypal.creditCard.create(card_data, function(error, credit_card){
-	  	if (error) {
-		    res.json({
-		        msg: 'inquiry table delet',
-		        status: 0,
-		        data: error
-		    });
-	  	} 
-	  	else 
-	  	{
-	  		var new_pack = new cards({
-			    userId:   req.body.external_customer_id,
-			    card_data:   credit_card,
-			    created_at: new Date()
-			});
+		paypal.creditCard.create(card_data, function(error, credit_card){
+		  	if (error) {
+			    res.json({
+			        msg: 'inquiry table delet',
+			        status: 0,
+			        data: error
+			    });
+		  	} 
+		  	else 
+		  	{
+		  		var new_pack = new cards({
+				    userId:   req.body.external_customer_id,
+				    card_data:   credit_card,
+				    created_at: new Date()
+				});
 
-		  	new_pack.save(function(err, doc){
-			    if(doc == null){
-			      res.send({
-			        data: null,
-			        error: 'Something went wrong.Please try later.',
-			        status: 0
-			      });
-			    }else{
-			      res.send({
-			        data: doc,
-			        status: 1,
-			        error: 'Testimonial added successfully!'
-			      });
-			    }
-			});
-	  	}
-	});
+			  	new_pack.save(function(err, doc){
+				    if(doc == null){
+				      res.send({
+				        data: null,
+				        error: 'Something went wrong.Please try later.',
+				        status: 0
+				      });
+				    }else{
+				      res.send({
+				        data: doc,
+				        status: 1,
+				        error: 'Testimonial added successfully!'
+				      });
+				    }
+				});
+		  	}
+		});
+  	});
+
+	
 };
 
 
