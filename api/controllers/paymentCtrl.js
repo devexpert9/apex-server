c180edc1326ad14845f35a7058daf863704e3695
root <manmohitindiit@gmail.com>
2020-11-09 15:25:17 +0530

    paypal details

--- api/controllers/paymentCtrl.js
+++ api/controllers/paymentCtrl.js
@@ -46,7 +46,7 @@ exports.storeCreditCardVault = function (req, res) {
     discover = new RegExp("^(6011|622(12[6-9]|1[3-9][0-9]|[2-8][0-9]{2}|9[0-1][0-9]|92[0-5]|64[4-9])|65)");
     if (number.match(re) != null){
         card_type = "Visa";
-    }else if (/^(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))$/.test(number)) 
+    }else if (/^(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))$/.test(number)){ 
      	// Mastercard 
     	// Updated for Mastercard 2017 BINs expansion
         card_type = "Mastercard";
