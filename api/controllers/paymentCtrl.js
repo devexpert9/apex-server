f65b24c41a9f7af7ee04c0551314176a8f799b07
root <manmohitindiit@gmail.com>
2020-11-09 21:23:57 +0530

    dxfdsf

--- api/controllers/paymentCtrl.js
+++ api/controllers/paymentCtrl.js
@@ -53,7 +53,7 @@ exports.storeCreditCardVault = function (req, res) {
  	visae = new RegExp("^(4026|417500|4508|4844|491(3|7))"),
     discover = new RegExp("^(6011|622(12[6-9]|1[3-9][0-9]|[2-8][0-9]{2}|9[0-1][0-9]|92[0-5]|64[4-9])|65)");
     if (number.match(re) != null){
-        card_type = "Visa";
+        card_type = "visa";
     }else if (/^(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))$/.test(number)){ 
      	// Mastercard 
     	// Updated for Mastercard 2017 BINs expansion
