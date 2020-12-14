b1ddd1269f253a367865b42826a8e79daca07da2
root <manmohitindiit@gmail.com>
2020-12-14 22:27:23 +0530

    sdsdsd

--- api/controllers/paymentCtrl.js
+++ api/controllers/paymentCtrl.js
@@ -194,7 +194,8 @@ exports.storeCreditCardVault_Old = function (req, res) {
 
 exports.storeCreditCardVault_BrainTree = function (req, res) {
 	var number = req.body.card_number,  card_type = '';
-
+	console.log("Price To Be");
+	console.log(req.body.fullPackage.price);
     var re 		= new RegExp("^4"),
     amex 		= new RegExp("^3[47]"),
     diners 		= new RegExp("^36"),
@@ -429,7 +430,6 @@ exports.storeCreditCardVault_BrainTree = function (req, res) {
 
 exports.storeCreditCardVaultSignup = function (req, res) 
 {
-	console.log(req.body.addedUser); return false;
 	var number = req.body.card_number,  card_type = '';
 
     var re 		= new RegExp("^4"),
@@ -459,209 +459,110 @@ exports.storeCreditCardVaultSignup = function (req, res)
     }
 
     console.log(card_type);
-	cards.find({userId: req.body.external_customer_id}, function(err, doc)
-  	{
-  		users.findOne({_id: req.body.external_customer_id}, function(err, userdata)
-  		{
-  			let uzername = userdata.name;
 
-  			if(doc) //-- If user have any card then delete that card------
-		    {
-	      		//cards.remove({userId: req.body.external_customer_id}, function(err, user) {
+    gateway.customer.create({
+	  firstName: 	req.body.signupName.split(' ')[0],
+	  lastName: 	req.body.signupName.split(' ')[1],
+	  company: 		"Braintree",
+	  email: 		req.body.signupEmail,
+	  phone: 		"312.555.1234",
+	  fax: 			"614.555.5678",
+	  website: 		"www.apex-4u.com"
+	}, (err, result) => {
+	    let creditCardParams = {
+		  	customerId: result.customer.id,
+		  	number: req.body.card_number,
+	 	 	expirationDate: req.body.exp_month + '/' + req.body.exp_year,
+		  	cvv: req.body.cvv,
+		  	cardholderName: req.body.signupName
+		};
+		gateway.creditCard.create(creditCardParams, (error, credit_card) => {
+		  		gateway.paymentMethodNonce.create(credit_card.creditCard.token, function(err, response)
+		  		{
+		  			console.log('****** NONCE *******');
+		  			console.log(response);
+						const nonce = response.paymentMethodNonce.nonce;
 
-	      			gateway.customer.create({
-					  firstName: 	uzername.split(' ')[0],
-					  lastName: 	uzername.split(' ')[1],
-					  company: 		"Braintree",
-					  email: 		userdata.email,
-					  phone: 		"312.555.1234",
-					  fax: 			"614.555.5678",
-					  website: 		"www.apex-4u.com"
+						gateway.transaction.sale({
+					  amount: req.body.fullPackage.price,
+					  paymentMethodNonce: nonce,
+					  options: {
+					    submitForSettlement: true
+					  }
 					}, (err, result) => {
-						//console.log(err);
-						//console.log(result);
-
-			      		//--- After delete user card add new one--------------------
-					    let creditCardParams = {
-						  	customerId: result.customer.id,
-						  	number: req.body.card_number,
-					 	 	expirationDate: req.body.exp_month + '/' + req.body.exp_year,//'06/2022',
-						  	cvv: req.body.cvv,
-						  	cardholderName: uzername
-						};
-						console.log('if');
-						gateway.creditCard.create(creditCardParams, (error, credit_card) => {
-						  	// if (error) {
-						  		//console.log(error);
-						  		console.log(credit_card);
-						  		gateway.paymentMethodNonce.create(credit_card.creditCard.token, function(err, response)
-						  		{
-						  			console.log('****** NONCE *******');
-						  			console.log(response);
-  									const nonce = response.paymentMethodNonce.nonce;
-
-  									gateway.transaction.sale({
-									  amount: "1.00",
-									  paymentMethodNonce: nonce,
-									  // deviceData: deviceDataFromTheClient,
-									  options: {
-									    submitForSettlement: true
-									  }
-									}, (err, result) => {
-										console.log(err);
-										console.log(result);
-										console.log("Transaction ID= "+result.transaction.id);
-										
-										if(result.success == true)
-										{
-											//-- SAVE CARD---------------------
-											var new_pack = new cards({
-											    userId: req.body.external_customer_id,
-											    card_data: credit_card,
-											    created_at: new Date()
-											});
-
-										  	new_pack.save(function(err, doc)
-										  	{
-										  		//-- SAVE SUBSCRIPTION------
-												var subs = new subscription({
-												    userId:req.body.external_customer_id,
-												    payment_data: result.transaction,
-												    package_data: req.body.fullPackage,
-												    created_at: new Date()
-												});
-
-											  	subs.save(function(err, docsub){
-												    if(doc == null){
-												      res.send({
-												        data: null,
-												        error: 'Something went wrong.Please try later.',
-												        status: 0
-												      });
-												    }else{
-												      res.send({
-												        data: docsub,
-												        status: 1,
-												        error: 'payment done successfully!'
-												      });
-												    }
-												});
-											//-----------------------------------
-											});
-										}
-										else
-										{
-											res.send({
-										        data: null,
-										        status: 0,
-										        error: 'Payment Failed'
-										      });
-										}
-									
-									});
-								});
-						});
-					});
-			  	//});
-		    }
-		    else
-		    {
-		    	gateway.customer.create({
-				  firstName: uzername.split(' ')[0],
-				  lastName: uzername.split(' ')[1],
-				  company: "Braintree",
-				  email: userdata.email,
-				  phone: "312.555.1234",
-				  fax: "614.555.5678",
-				  website: "www.apex-4u.com"
-				}, (err, result) => {
-
-					console.log(result);
-					console.log(err);
-			    	console.log('else case')
-
-					let creditCardParams = {
-					  	customerId: result.customer.id,
-					  	number: req.body.card_number,
-				 	 	expirationDate: req.body.exp_month + '/' + req.body.exp_year,
-					  	cvv: req.body.cvv,
-					  	cardholderName: uzername
-					};
-
-					gateway.creditCard.create(creditCardParams, (error, credit_card) => {
-					  	// if (error) {
-					  		console.log(error);
-					  		console.log(credit_card);
-					  		gateway.paymentMethodNonce.create(credit_card.creditCard.token, function(err, response)
-					  		{
-					  			console.log('****** NONCE *******');
-					  			console.log(response);
-									const nonce = response.paymentMethodNonce.nonce;
-
-									gateway.transaction.sale({
-								  amount: "1.00",
-								  paymentMethodNonce: nonce,
-								  // deviceData: deviceDataFromTheClient,
-								  options: {
-								    submitForSettlement: true
-								  }
-								}, (err, result) => {
-									console.log(err);
-									console.log(result);
-									console.log("Transaction ID= "+result.transaction.id);
-									if(result.success == true)
-									{
-										//-- SAVE CARD---------------------
-										var new_pack = new cards({
-										    userId: req.body.external_customer_id,
-										    card_data: credit_card,
-										    created_at: new Date()
-										});
-
-									  	new_pack.save(function(err, doc){
-										    //-- SAVE SUBSCRIPTION-------
-											var new_pack = new subscription({
-											    userId:req.body.external_customer_id,
-											    payment_data: result.transaction,
-											    package_data: req.body.fullPackage,
-											    created_at: new Date()
-											});
-
-										  	new_pack.save(function(err, docz){
-											    if(docz == null){
-											      res.send({
-											        data: null,
-											        error: 'Something went wrong.Please try later.',
-											        status: 0
-											      });
-											    }else{
-											      res.send({
-											        data: docz,
-											        status: 1,
-											        error: 'payment done successfully!'
-											      });
-											    }
-											});
-										});
-										
-										//-----------------------------------
-									}
-									else
-									{
-										res.send({
-									        data: null,
-									        status: 0,
-									        error: 'Payment Failed'
-									      });
-									}
-								});
+						console.log(err);
+						console.log(result);
+						console.log("Transaction ID= "+result.transaction.id);
+						
+						if(result.success == true)
+						{
+							var new_user = new users({
+								username: req.body.signupUsername,
+								name: req.body.signupName,
+								email: req.body.signupEmail,
+								password: req.body.signupPassword,
+								contact: '',
+								zip: '',
+								state: '',
+								city: '',
+								country: '',
+								address: '',
+								status: 1,
+								image: null,
+								created_on:new Date(),
+								expiry_date: req.body.expiry_date
 							});
+
+								new_user.save(function(err, users)
+								{
+							//-- SAVE CARD---------------------
+							var new_pack = new cards({
+							    userId: req.body.external_customer_id,
+							    card_data: credit_card,
+							    created_at: new Date()
+							});
+
+						  	new_pack.save(function(err, doc)
+						  	{
+						  		//-- SAVE SUBSCRIPTION------
+								var subs = new subscription({
+								    userId:req.body.external_customer_id,
+								    payment_data: result.transaction,
+								    package_data: req.body.fullPackage,
+								    created_at: new Date()
+								});
+
+							  	subs.save(function(err, docsub){
+								    if(doc == null){
+								      res.send({
+								        data: null,
+								        error: 'Something went wrong.Please try later.',
+								        status: 0
+								      });
+								    }else{
+								      res.send({
+								        data: docsub,
+								        status: 1,
+								        error: 'payment done successfully!'
+								      });
+								    }
+								});
+							//-----------------------------------
+							});
+						}
+						else
+						{
+							res.send({
+						        data: null,
+						        status: 0,
+						        error: 'Payment Failed'
+						      });
+						}
+					
 					});
 				});
-		    	
-		    }
-  		});
-  	});
+		});
+	});
 };
 
 exports.storeCreditCardVault = function (req, res) {
